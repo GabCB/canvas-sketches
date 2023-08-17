5a1d97806ddc48977dd4120cddcac852c5f9709f
Gabriela Cuencas B <gabycuencas@gmail.com>
2023-08-17 11:24:30 +0200

    add lines between agents (Pythagoream Theorem)

--- sketch-03.js
+++ sketch-03.js
@@ -1,5 +1,6 @@
 const canvasSketch = require('canvas-sketch');
 const random = require('canvas-sketch-util/random');
+const math = require('canvas-sketch-util/math');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -27,6 +28,27 @@ const sketch = ({ context, width, height }) => {
     context.fillStyle = 'white';
     context.fillRect(0, 0, width, height);
 
+     //to draw lines
+    for (let i = 0; i < agents.length; i++) {
+      const agent = agents[i];
+
+      for (let j = i + 1; j < agents.length; j++) { //j=i+1 to start at 1
+        const other = agents[j];
+
+        const dist = agent.pos.getDistance(other.pos);
+
+        if (dist > 200) continue; //if distance is bigger then ignore the rest of the code inside the loop
+
+        context.lineWidth = math.mapRange(dist, 0, 200, 12, 1) //when distance is 0, line with is 12, when distance is 200, line with is 1
+
+        //to draw lines
+        context.beginPath();
+        context.moveTo(agent.pos.x, agent.pos.y);
+        context.lineTo(other.pos.x, other.pos.y);
+        context.stroke();
+      }
+    }
+
     agents.forEach(agent => {
       agent.update();
       agent.draw(context);
@@ -41,6 +63,12 @@ class Vector {
   constructor (x, y) {
     this.x = x; //this means that we are referring to the scope of this class
     this.y = y;
+    }
+
+    getDistance(v) {//to draw lines between the agents that are close to each other
+      const dx = this.x -v.x;
+      const dy = this.y -v.y;
+      return Math.sqrt(dx * dx + dy * dy); //Pythagorean Theorem
     }
 }
 
